eacd62182f9ec24855a63bbd10b7e94afa6c807e
Rogério Sandim <sandimroger@gmail.com>
2018-12-07 10:57:24 -0200

    Add comments on .js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Global variables 
 const clock = document.querySelector('.clock');
 const deck = document.querySelector('.deck');
 const restart = document.querySelector('.restart');
@@ -12,6 +13,7 @@ let cardsOpened = [];
 let moves = 0;
 let totalTimePlayed;
 
+// This function is responsible to check if a method is iterative.
 function isInteractive(obj) {
     if (obj == null) {
         return false;
@@ -35,6 +37,7 @@ function shuffle(array) {
 }
 
 let card = {
+// This method is responsible to remove or add classes on a card that has been selected.
     toggle(cardsSelected, classes) {
         if (isInteractive(cardsSelected)) {
             cardsSelected.forEach(cardSelected => {
@@ -52,15 +55,18 @@ let card = {
 };
 
 const gameServices = {
+// This method is responsible to start the clock
     clockStart() {
         let clockStart = new Date().getTime();
         timer = setInterval(function () {
             var now = new Date().getTime();
             var lapse = now - clockStart;
 
+// Calculation the minutes and seconds.
             let minutes = Math.floor((lapse % (1000 * 60 * 60)) / (1000 * 60));
             let seconds = Math.floor((lapse % (1000 * 60)) / 1000);
 
+// Add 0 on the left once seconds and minutes are less than 10.
             if (seconds < 10) {
                 seconds = "0" + seconds;
             }
@@ -74,6 +80,7 @@ const gameServices = {
         }, 750);
     },
 
+// This method is responsible to increment movements once you select 2 cards.
     checkCards() {
         cardsOpened = document.querySelectorAll('.open');
         if (cardsOpened.length === 2) {
@@ -83,6 +90,7 @@ const gameServices = {
         }
     },
 
+// This method is responsible to check if two cards are a match.
     matchedCards() {
         let openCard = cardsOpened;
         const isMatch = cardsOpened[0].isEqualNode(openCard[1]);
@@ -96,6 +104,7 @@ const gameServices = {
         }
     },
 
+// This method is responsible to define your score based on movements.
     updateScore() {
         const excellentScore = 12;
         const movesElement = document.querySelector('.moves');
@@ -113,8 +122,9 @@ const gameServices = {
 };
 
 const inGameActions = {
+// This method is responsible to suffle the cards.
     shuffleCards() {
-        clock.innerHTML = "00:00"
+        clock.innerHTML = "00:00";
         const cardsShuffled = shuffle(listOfCards);
         deck.innerHTML = '';
         gameServices.updateScore();
@@ -123,6 +133,7 @@ const inGameActions = {
         });
     },
 
+// This method is responsible to iniciate the game once you click in any card.    
     initiateGame() {
         deck.addEventListener('click', event => {
             if (!startGame) {
@@ -141,6 +152,7 @@ const inGameActions = {
         });
     },
 
+// This method is responsible to restart the moves, the time and the score.
     restartGame() {
         const numberOfStars = document.getElementsByClassName('fa-star');
         moves = 0;
@@ -156,8 +168,10 @@ const inGameActions = {
         inGameActions.shuffleCards();
     },
 
+// This method is responsible to check if all the matches were found.
     endGame() {
         const matchedCards = document.getElementsByClassName('match');
+// In case you found all the matches you will the game. 
         if (matchedCards.length === 16) {
             clearInterval(timer);
             showScore({
@@ -166,6 +180,7 @@ const inGameActions = {
                 resultMessage: 'You could find all the matches!'
             })
         }
+// In case that moves are iqual to 30 the game is over.
         if (moves === 30) {
             clearInterval(timer);
             showScore({
@@ -177,6 +192,7 @@ const inGameActions = {
     }
 };
 
+// Global variables for the score result modal.
 const result = document.querySelector('.result');
 const title = result.querySelector('.title');
 const message = result.querySelector('.message');
@@ -184,6 +200,7 @@ const movesScore = result.querySelector('.moves-score');
 const timerScore = result.querySelector('.timer-score');
 const restartButton = result.querySelector('.button');
 
+// this function is responsible to show the score result modal.
 function showScore({ resultTitle, classTitle, resultMessage }) {
     result.classList.add('show');
     title.innerHTML = resultTitle;
